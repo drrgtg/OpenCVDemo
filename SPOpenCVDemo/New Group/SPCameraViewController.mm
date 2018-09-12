3c21367fbddaf5eda9a7adbd16f6d948470aa070
drrgtg <412032320@qq.com>
2018-09-12 17:19:43 +0800

    for camera

--- SPOpenCVDemo/New Group/SPCameraViewController.mm
+++ SPOpenCVDemo/New Group/SPCameraViewController.mm
@@ -23,7 +23,7 @@ using namespace cv;
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     self.videoCamera = [[CvVideoCamera alloc] initWithParentView:self.imageView];
-    self.videoCamera.defaultAVCaptureDevicePosition = AVCaptureDevicePositionBack;
+    self.videoCamera.defaultAVCaptureDevicePosition = AVCaptureDevicePositionFront;
     self.videoCamera.defaultAVCaptureSessionPreset = AVCaptureSessionPreset352x288;
     self.videoCamera.defaultAVCaptureVideoOrientation = AVCaptureVideoOrientationPortrait;
     self.videoCamera.defaultFPS = 60;
@@ -44,13 +44,13 @@ using namespace cv;
 {
     // Do some OpenCV stuff with the image
     Mat image_copy;
-    cvtColor(image, image_copy, COLOR_BGR2GRAY);
+    cvtColor(image, image_copy, COLOR_BGR2GRAY);// bgr to gray
     // invert image
     bitwise_not(image_copy, image_copy);
     //Convert BGR to BGRA (three channel to four channel)
     Mat bgr;
-    cvtColor(image_copy, bgr, COLOR_GRAY2BGR);
-    cvtColor(bgr, image, COLOR_BGR2BGRA);
+    cvtColor(image_copy, bgr, COLOR_GRAY2BGR);// gray to bgr
+    cvtColor(bgr, image, COLOR_BGR2BGRA);// bgr to bgra
 }
 
 #pragma mark - UI Actions
